025d5916c71f10ee016a875586da18a6f4a09c89
Mihail Savvopoulos <m_savvop@otenet.gr>
2018-06-07 15:30:44 +0300

    added a subroutine to find which OS is used to compile the program  and which windows version
    In this commit (June 7, 2018) I added a subroutine to find which OS is used to compile the program
    and which windows version it is if the OS is windows.

--- filetool_el.ts
+++ filetool_el.ts
@@ -2,6 +2,47 @@
 <!DOCTYPE TS>
 <TS version="2.1" language="el_GR">
 <context>
+    <name>OverwriteDialog</name>
+    <message>
+        <location filename="overwritedialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished">Διάλογος</translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.ui" line="26"/>
+        <source>TextLabel</source>
+        <translation type="unfinished">Ετικέτα</translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.ui" line="42"/>
+        <source>Yes</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.ui" line="49"/>
+        <source>YesToAll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.ui" line="56"/>
+        <source>No</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.ui" line="63"/>
+        <source>NoToAll</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="overwritedialog.cpp" line="26"/>
+        <source>File 
+%2 
+exists, should I overwrite it  with file 
+ %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
     <name>PasteDialog</name>
     <message>
         <location filename="pastedialog.ui" line="14"/>
@@ -19,17 +60,25 @@
         <translation>Ακύρωσε</translation>
     </message>
     <message>
-        <location filename="pastedialog.cpp" line="32"/>
-        <source>%1</source>
+        <location filename="pastedialog.cpp" line="62"/>
+        <source>%1
+  %2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="pastedialog.cpp" line="48"/>
+        <location filename="pastedialog.cpp" line="67"/>
+        <source>%1
+  %2 
+ to %3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="pastedialog.cpp" line="103"/>
         <source>Confirm Canceling Current Action</source>
         <translation>Επιβεβαίωσε Ακύρωση Τρέχουσας Ενέργειας</translation>
     </message>
     <message>
-        <location filename="pastedialog.cpp" line="48"/>
+        <location filename="pastedialog.cpp" line="103"/>
         <source>Are you sure that you want to Stop Current Action?</source>
         <translation>Είσαι σίγουρος ότι θέλεις να σταματήσεις την τρέχουσα ενέργεια;</translation>
     </message>
@@ -37,232 +86,266 @@
 <context>
     <name>filetool</name>
     <message>
-        <location filename="filetool.ui" line="14"/>
+        <location filename="filetool.ui" line="20"/>
         <source>filetool</source>
         <translation>Εξερευνητής</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="29"/>
+        <location filename="filetool.ui" line="35"/>
         <source>Go to</source>
         <translation>Πήγαινε</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="159"/>
+        <location filename="filetool.ui" line="165"/>
         <source>&amp;File</source>
         <translation>&amp;Αρχείο</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="163"/>
+        <location filename="filetool.ui" line="169"/>
         <source>&amp;new</source>
         <translation>&amp;Νέο</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="174"/>
+        <location filename="filetool.ui" line="180"/>
         <source>&amp;Edit</source>
         <translation>&amp;Επεξεργασία</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="188"/>
+        <location filename="filetool.ui" line="195"/>
         <source>View</source>
         <translation>Προβολή</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="196"/>
+        <location filename="filetool.ui" line="203"/>
         <source>Help</source>
         <translation>Βοήθεια</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="250"/>
+        <location filename="filetool.ui" line="258"/>
         <source>&amp;cut</source>
         <translation>Απο&amp;κοπή</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="253"/>
+        <location filename="filetool.ui" line="261"/>
         <source>Ctrl+X</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="262"/>
+        <location filename="filetool.ui" line="270"/>
         <source>c&amp;opy</source>
         <translation>&amp;Αντιγραφή</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="265"/>
+        <location filename="filetool.ui" line="273"/>
         <source>Ctrl+C</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="274"/>
+        <location filename="filetool.ui" line="282"/>
         <source>&amp;paste</source>
         <translation>&amp;Επικόληση</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="277"/>
+        <location filename="filetool.ui" line="285"/>
         <source>Ctrl+V</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="286"/>
+        <location filename="filetool.ui" line="294"/>
         <source>&amp;undo</source>
         <translation>Α&amp;ναίρεση</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="289"/>
+        <location filename="filetool.ui" line="297"/>
         <source>Ctrl+Z</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="298"/>
+        <location filename="filetool.ui" line="306"/>
         <source>re&amp;do</source>
         <translation>&amp;Επανάληψη</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="301"/>
+        <location filename="filetool.ui" line="309"/>
         <source>Ctrl+Y</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="310"/>
+        <location filename="filetool.ui" line="318"/>
         <source>&amp;delete</source>
         <translation>&amp;Διαγραφή</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="313"/>
+        <location filename="filetool.ui" line="321"/>
+        <source>delete is permanent and cannot be undone</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filetool.ui" line="324"/>
         <source>Ctrl+D</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="322"/>
+        <location filename="filetool.ui" line="333"/>
         <source>&amp;rename</source>
         <translation>&amp;Μετονομασία</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="325"/>
+        <location filename="filetool.ui" line="336"/>
         <source>F2</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="334"/>
+        <location filename="filetool.ui" line="345"/>
         <source>file</source>
         <translation>Αρχείο</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="337"/>
+        <location filename="filetool.ui" line="348"/>
         <source>create file</source>
         <translation>Δημιουργία Αρχείου</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="346"/>
+        <location filename="filetool.ui" line="357"/>
         <source>directory</source>
         <translation>Κατάλογος</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="349"/>
+        <location filename="filetool.ui" line="360"/>
         <source>create dir</source>
         <translation>Δημιουργία καταλόγου</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="352"/>
+        <location filename="filetool.ui" line="363"/>
         <source>Ctrl+Shift+N</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="361"/>
+        <location filename="filetool.ui" line="372"/>
         <source>&amp;open</source>
         <translation>Άν&amp;οιγμα</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="370"/>
+        <location filename="filetool.ui" line="381"/>
         <source>&amp;exit</source>
         <translation>Έ&amp;ξοδος</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="379"/>
+        <location filename="filetool.ui" line="390"/>
         <source>about...</source>
         <translation>&amp;Περί</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="388"/>
+        <location filename="filetool.ui" line="399"/>
         <source>List View</source>
         <translation>Προβολή Λίστας</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="397"/>
+        <location filename="filetool.ui" line="408"/>
         <source>Icon View</source>
         <translation>Προβολή Εικόνων</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="406"/>
+        <location filename="filetool.ui" line="417"/>
         <source>previous</source>
         <translation>Προηγούμενο</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="415"/>
+        <location filename="filetool.ui" line="426"/>
         <source>next</source>
         <translation>Επόμενο</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="424"/>
+        <location filename="filetool.ui" line="435"/>
         <source>parentDir</source>
         <translation>Προηγούμενος κατάλογος</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="433"/>
+        <location filename="filetool.ui" line="444"/>
         <source>go_home</source>
         <translation>Πήγαινε στην Αρχή</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="436"/>
+        <location filename="filetool.ui" line="447"/>
         <source>home</source>
         <translation>Αρχική Σελίδα</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="445"/>
+        <location filename="filetool.ui" line="456"/>
         <source>List Details View</source>
         <translation>Προβολή Λεπτομεριών</translation>
     </message>
     <message>
-        <location filename="filetool.ui" line="448"/>
+        <location filename="filetool.ui" line="459"/>
         <source>List Details</source>
         <translation>Λεπτομέριες</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="285"/>
+        <location filename="filetool.ui" line="468"/>
+        <source>recycle</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filetool.ui" line="471"/>
+        <source>recycle is not implemented yet</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filetool.cpp" line="310"/>
         <source>listIndex</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="285"/>
+        <location filename="filetool.cpp" line="310"/>
         <source>listIndex = %1</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="285"/>
+        <location filename="filetool.cpp" line="310"/>
         <source>ok</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="529"/>
+        <location filename="filetool.cpp" line="633"/>
         <source>MyComputer:</source>
         <translation>Ο Υπολογιστής μου:</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="577"/>
+        <location filename="filetool.cpp" line="684"/>
         <source>Confirm Deletion</source>
         <translation>Επιβεβαίωσε Διαργαφή</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="577"/>
-        <source>Are you sure that you want to Delete %1 items?</source>
-        <translation>Είσαι σίγουρος ότι θέλεις να Διαγράψεις %1 αντικείμενα;</translation>
+        <location filename="filetool.cpp" line="684"/>
+        <source>Are you sure that you want to Delete %1 items? Removing them is permanent and cannot be undone</source>
+        <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="607"/>
+        <location filename="filetool.cpp" line="799"/>
+        <source>This Program is Created by Mihail Savvopoulos using Qt. The icons were  taken from Linux. (Default KDE icons from Mint.)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="filetool.cpp" line="1663"/>
+        <source>Windows filetool</source>
+        <translation type="unfinished">Εξερευνητής των Windows</translation>
+    </message>
+    <message>
+        <location filename="filetool.cpp" line="1707"/>
+        <source>Linux filetool</source>
+        <translation type="unfinished">Εξερευνητής του Linux</translation>
+    </message>
+    <message>
+        <source>Are you sure that you want to Delete %1 items?</source>
+        <translation type="vanished">Είσαι σίγουρος ότι θέλεις να Διαγράψεις %1 αντικείμενα;</translation>
+    </message>
+    <message>
+        <location filename="filetool.cpp" line="707"/>
         <source>Are you sure that you want to move %1 items?</source>
         <translation>Είσαι σίγουρος ότι θέλεις να μετακινήσεις %1 αντικείμενα;</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="636"/>
+        <location filename="filetool.cpp" line="726"/>
         <source>Are you sure that you want to copy %1 items?</source>
         <translation>Είσαι σίγουρος ότι θέλεις να αντιγράψεις %1 αντικείμενα;</translation>
     </message>
@@ -275,7 +358,7 @@
         <translation type="vanished">Είσαι σίγουρος ότι θέλεις να Διαγράψεις %1 αντικείμενα;</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="607"/>
+        <location filename="filetool.cpp" line="707"/>
         <source>Confirm move action</source>
         <translation>Επιβεβαίωσε Αποκοπή</translation>
     </message>
@@ -284,7 +367,7 @@
         <translation type="vanished">Είσαι σίγουρος ότι θέλεις να μετακινήσεις %1 αντικείμενα;</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="636"/>
+        <location filename="filetool.cpp" line="726"/>
         <source>Confirm copy action</source>
         <translation>Επιβεβαίωσε Αντιγραφή</translation>
     </message>
@@ -293,14 +376,13 @@
         <translation type="vanished">Είσαι σίγουρος ότι θέλεις να αντιγράψεις %1 αντικείμενα;</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="707"/>
+        <location filename="filetool.cpp" line="799"/>
         <source>Filetool</source>
         <translation type="unfinished">Εξερευνητής</translation>
     </message>
     <message>
-        <location filename="filetool.cpp" line="707"/>
         <source>This Program is Created by Mihail Savvopoulos using Qt</source>
-        <translation>Αυτό το πρόγραμμα δημιουργήθηκε από τον Μιχαήλ Σαββόπουλο με την χρήση της βιβλιοθήκης  Qt</translation>
+        <translation type="vanished">Αυτό το πρόγραμμα δημιουργήθηκε από τον Μιχαήλ Σαββόπουλο με την χρήση της βιβλιοθήκης  Qt</translation>
     </message>
 </context>
 </TS>
